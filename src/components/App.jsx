ef537035e7b93e1c40d574e309eb86027ce1d121
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2024-01-23 23:30:20 +0200

    task-img-huk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { searchImg } from './api/getAllImg';
 
 import Searchbar from './searchbar/Searchbar';
@@ -8,91 +8,68 @@ import ImageGallery from './imageGallery/ImageGallery';
 import style from './app.module.css';
 import Modal from './modal/Modal';
 
-class App extends Component {
-  state = {
-    search: '',
-    item: [],
-    loading: false,
-    error: null,
-    page: 1,
-    imgDetails: {},
-    modalOpen: false,
-  };
+const App = () => {
+  const [search, setSearch] = useState('');
+  const [item, setItem] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [imgDetails, setImgDetails] = useState({});
+  const [modalOpen, setModalOpen] = useState(false);
+  const [isItem, setIsItem] = useState(false);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { search, page } = this.state;
+  useEffect(() => {
+    if (search === '') return;
+    fetchImage();
+  }, [search, page]);
 
-    if (search !== prevState.search || page !== prevState.page) {
-      this.fetchImage();
+  const fetchImage = async () => {
+    setLoading(true);
+    try {
+      const { data } = await searchImg(search, page);
+      setIsItem(true);
+      const array = data.hits;
+      setItem(prevItems => [...prevItems, ...array]);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
     }
-  }
-
-  async fetchImage() {
-    const { search, page } = this.state;
-    this.setState({ loading: true });
-    searchImg(search, page)
-      .then(({ data }) => {
-        const array = data.hits;
-        this.setState(({ item }) => ({
-          item: array?.length ? [...item, ...array] : item,
-        }));
-      })
-      .catch(error => {
-        this.setState({
-          error: error.message,
-        });
-      })
-      .finally(() => {
-        this.setState({
-          loading: false,
-        });
-      });
-  }
-
-  loadMore = () => {
-    this.setState(({ page }) => ({ page: page + 1 }));
   };
 
-  showModal = ({ largeImageURL }) => {
-    this.setState({
-      modalOpen: true,
-      imgDetails: { largeImageURL },
-    });
+  const loadMore = () => {
+    setPage(page + 1);
   };
 
-  closeModal = () =>
-    this.setState({
-      modalOpen: false,
-      imgDetails: {},
-    });
-
-  hendleSearch = ({ search }) => {
-    this.setState({
-      search,
-      item: [],
-      page: 1,
-    });
+  const showModal = ({ largeImageURL }) => {
+    setModalOpen(true);
+    setImgDetails({ largeImageURL });
   };
 
-  render() {
-    const { loadMore, hendleSearch, showModal, closeModal } = this;
-    const { item, loading, imgDetails, modalOpen } = this.state;
+  const closeModal = () => {
+    setModalOpen(false);
+    setImgDetails({});
+  };
 
-    const isItem = Boolean(item.length);
-    return (
-      <div className={style.box}>
-        <Searchbar onSubmit={hendleSearch} />
-        {isItem && <ImageGallery items={item} showModal={showModal} />}
-        <Loader loading={loading} />
-        {isItem && (
-          <Button onClick={loadMore} type="button">
-            Load More
-          </Button>
-        )}
-        {modalOpen && <Modal close={closeModal} img={imgDetails}></Modal>}
-      </div>
-    );
-  }
-}
+  const hendleSearch = ({ search }) => {
+    setSearch(search);
+    setItem([]);
+    setPage(1);
+    console.log(item, page, search);
+  };
+  return (
+    <div className={style.box}>
+      <Searchbar onSubmit={hendleSearch} />
+      {isItem && <ImageGallery items={item} showModal={showModal} />}
+      <Loader loading={loading} />
+      {isItem && (
+        <Button onClick={loadMore} type="button">
+          Load More
+        </Button>
+      )}
+      {modalOpen && <Modal close={closeModal} img={imgDetails}></Modal>}
+    </div>
+  );
+};
 
 export default App;
--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,51 +1,42 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import style from './searchbar.module.css';
 
-class Searchbar extends Component {
-  state = {
-    search: '',
+const Searchbar = ({ onSubmit }) => {
+  const [search, setSearch] = useState('');
+
+  const handleChange = ({ target }) => {
+    const { value } = target;
+    setSearch(value);
   };
 
-  //для запису значення з форми
-  handleChange = ({ target }) => {
-    const { name, value } = target;
-    this.setState({ [name]: value });
-  };
-
-  // для відправки форми
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    // можливо не потрібно розпилення
-    this.props.onSubmit({ ...this.state });
-    this.setState({ search: '' });
+    onSubmit({ search });
+    setSearch('');
   };
 
-  render() {
-    const { handleChange, handleSubmit } = this;
-    const { search } = this.state;
-    return (
-      <header className={style.searchbar}>
-        <form onSubmit={handleSubmit} className={style.searchForm}>
-          <button type="submit" className={style.searchFormButton}>
-            <span className={style.searchFormButtonLabel}>Search</span>
-          </button>
+  return (
+    <header className={style.searchbar}>
+      <form onSubmit={handleSubmit} className={style.searchForm}>
+        <button type="submit" className={style.searchFormButton}>
+          <span className={style.searchFormButtonLabel}>Search</span>
+        </button>
 
-          <input
-            name="search"
-            value={search}
-            onChange={handleChange}
-            className={style.searchFormInput}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            required
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          name="search"
+          value={search}
+          onChange={handleChange}
+          className={style.searchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          required
+        />
+      </form>
+    </header>
+  );
+};
 
 export default Searchbar;
